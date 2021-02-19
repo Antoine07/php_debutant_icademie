3c8adfc2d2f1d2c1b275a57d898bcc55c5bba9ed
Antoine07 <antoine.lucsko@gmail.com>
2021-02-19 19:07:05 +0100

    search number

--- Exercices/boucle_04.php
+++ Exercices/boucle_04.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Exercise
  *
@@ -16,14 +17,14 @@ print_r('<pre/>');
 
 
 print_r('<pre>');
-print_r( count($data) );
+print_r(count($data));
 print_r('<pre/>');
 
 // Parcourir le tableau 
 
 // start $i = 0; condition d'arrêt; pas pour avancer
 print_r('<pre>');
-for($i= 0; $i < 100  ; $i = $i + 1){
+for ($i = 0; $i < 100; $i = $i + 1) {
     print_r($i);
     print_r('<br />');
 }
@@ -32,40 +33,43 @@ print_r('<pre/>');
 // Affichez la 3eme valeur de $data array PHP
 
 print_r('<pre>');
-print_r( $data[2] ); // 2 c'est l'indice du tableau donc on accède à la 3eme valeur
+print_r($data[2]); // 2 c'est l'indice du tableau donc on accède à la 3eme valeur
 print_r('<pre/>');
 
 
 // Affichez la 6eme valeur de $data array PHP
 
 print_r('<pre>');
-print_r( $data[5] ); 
+print_r($data[5]);
 print_r('<pre/>');
 
 // Affichez la 13eme valeur de $data array PHP
 
 print_r('<pre>');
-print_r( $data[12] ); 
+print_r($data[12]);
 print_r('<pre/>');
 
 // Affichez la 7eme valeur de $data array PHP
 
 print_r('<pre>');
-print_r( $data[6] ); 
+print_r($data[6]);
 print_r('<pre/>');
 
 
 // Affichez la 10eme valeur de $data array PHP
 
 print_r('<pre>');
-print_r( $data[9] ); 
+print_r($data[9]);
 print_r('<pre/>');
 
 
 // Parcourir $data simplement ! 
 
 echo "<ul>";
-for($i = 0; $i < count($data) ; $i = $i + 1){
-    echo "<li>" . $data[ $i ] . "</li>"; // concaténation avec . pour rassembler une chaîne de caractères avec une variable
+for ($i = 0; $i < count($data); $i = $i + 1) {
+    echo "<li>" . $data[$i] . "</li>"; // concaténation avec . pour rassembler une chaîne de caractères avec une variable
 }
 echo "</ul>";
+
+
+$data_02 = [11, 18, 19, 9, 11, 100, 78, 876, 11];
